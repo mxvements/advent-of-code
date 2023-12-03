c478f2d7d1bee04a1a3861175ac1d4d9bf2361e9
mxvements <lucia.martindeaguilera@gmail.com>
2023-12-03 09:53:44 +0100

    added gnl

--- day01/day01.c
+++ day01/day01.c
@@ -6,16 +6,18 @@
 /*   By: lmmielgo <lmmielgo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 09:25:21 by luciama2          #+#    #+#             */
-/*   Updated: 2023/12/03 09:45:26 by lmmielgo         ###   ########.fr       */
+/*   Updated: 2023/12/03 09:53:26 by lmmielgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include "gnl/get_next_line.h"
 
 int	main(void)
 {
 	int	fd;
 
 	fd = open("puzzle-input", O_RDONLY);
+	
 	
 }
